64906929252f9ed2f90d1b829de442c9b1aace7b
Juan Pérez Gago <juan.perez@interacso.com>
2023-01-29 22:06:22 +0100

    allow draw rectangle stroke or fill

--- js/canvas.js
+++ js/canvas.js
@@ -1,9 +1,13 @@
 export default class Canvas {
   static ctx = document.querySelector('canvas').getContext('2d')
 
-  static drawRectangule (x, y, width, height, color = 'black') {
-    // this.ctx.fillStyle = color
-    // this.ctx.fillRect(x, y, width, height)
+  static drawRectangule ({ x, y, width, height, fill = true, color = 'black' }) {
+    if (fill) {
+      this.ctx.fillStyle = color
+      this.ctx.fillRect(x, y, width, height)
+      return
+    }
+
     this.ctx.strokeStyle = color
     this.ctx.strokeRect(x, y, width, height)
   }
